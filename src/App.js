4b6ab637cd22340b5809b178345f4c0a0e1c74a6
SuryaGSPL <surya@graspear.com>
2024-05-30 12:56:57 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import ProgressBar from './progress-bar/ProgressBar';
+import { Container } from '@mui/material';
+
+function App() {
+  return (
+    <div className="">
+      <Container>
+        <ProgressBar/>
+      </Container>
+      
+    </div>
+  );
+}
+
+export default App;
